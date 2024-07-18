4e393a39bbd907a3fe712bf7da99b872489a19ee
qugalet <qugalet@m0e.space>
2024-07-18 12:05:31 +0300

    add requirements + cache

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 #!/bin/python
 from flask import Flask, jsonify, request
+from flask_caching import Cache
 from waitress import serve
 from wtforms import Form, validators, StringField
 from app import read_number_plates
 
 app = Flask(__name__)
+app.config.from_mapping(
+    {
+        "DEBUG": True,  # some Flask specific configs
+        "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+        "CACHE_DEFAULT_TIMEOUT": 300,
+    }
+)
+cache = Cache(app)
 
 
-@app.route('/status')
+@app.route("/status")
 def status():
     return "alive"
 
 
 class ReadForm(Form):
-    url = StringField('Url to image', [
-        validators.URL()
-    ])
+    url = StringField("Url to image", [validators.URL()])
 
 
-@app.route('/read')
+@app.route("/read")
+@cache.cached(timeout=86400)
 def read():
     form = ReadForm(request.args)
 
@@ -28,17 +36,16 @@ def read():
 
         # print(number_plates, region_names, images_bboxs)
 
-        return jsonify({
-            'success': True,
-            'url': urls_result,
-            'number_plates': number_plates,
-            'region_names': region_names,
-        })
+        return jsonify(
+            {
+                "success": True,
+                "url": urls_result,
+                "number_plates": number_plates,
+                "region_names": region_names,
+            }
+        )
 
-    return jsonify({
-        'success': False,
-        'errors': form.errors
-    })
+    return jsonify({"success": False, "errors": form.errors})
 
 
 def create_app():
